7e3830ce9a0c14415e7835da6360d15c6f260862
Jerry Okolo <jerrycreative@live.co.uk>
2023-06-29 23:20:08 +0100

    add substreams entity change

--- src/pb/mod.rs
+++ src/pb/mod.rs
@@ -8,3 +8,12 @@ pub mod eth {
         }
     }
 }
+pub mod substreams {
+    pub mod entity {
+        // @@protoc_insertion_point(attribute:substreams.entity.v1)
+        pub mod v1 {
+            include!("substreams.entity.v1.rs");
+            // @@protoc_insertion_point(substreams.entity.v1)
+        }
+    }
+}
